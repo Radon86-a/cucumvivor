888882ace9f1f8421ea290edb6fd215205f5f7f2
Radon86 <aya10tomoi@gmail.com>
2026-06-28 04:17:17 +0900

    スコアの表示

--- Assets/Script/UI/Result.cs
+++ Assets/Script/UI/Result.cs
@@ -5,6 +5,7 @@ public class Result : MonoBehaviour
 {
     public GameObject result;
     public TextMeshProUGUI killText;
+    public TextMeshProUGUI scoreText;
     public gameData game_data;
     void Start()
     {
@@ -14,6 +15,7 @@ public class Result : MonoBehaviour
     {
         result.SetActive(true);
         killText.text = "撃破数:" + game_data.kill_enemy;
+        scoreText.text = "スコア:" + game_data.score;
         Time.timeScale = 0f;
     }
 }
