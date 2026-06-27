e9aaa7a4df6c615db4beabf19e33253ef25ecbcf
shika_h <shikano.h.1f33@m.isct.ac.jp>
2026-06-27 20:43:55 +0900

    change

---
+++ Assets/Script/sub/WeaponDisplay.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+
+public class WeaponDisplay : MonoBehaviour
+{
+    public weaponData weaponDatabase;   // ScriptableObject
+    public TextMeshProUGUI nameText;    // 武器名
+    public TextMeshProUGUI detailText;  // 説明文
+    public Image weaponImage;           // 画像
+
+    public void ShowWeapon(int id)
+    {
+        // 指定IDの武器データを取得
+        Weapons w = weaponDatabase.weapons[id];
+
+        // UI に反映
+        nameText.text = w.weapon_name;
+        detailText.text = w.weapon_detail;
+        weaponImage.sprite = w.weapon_skin;
+    }
+}
+
