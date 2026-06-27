462887b2d47121970f99d5854a25aa7506dd15db
Radon86 <aya10tomoi@gmail.com>
2026-06-27 09:25:53 +0900

    敵のデータをつくった

--- Assets/Script/enemyData.cs
+++ Assets/Script/enemyData.cs
@@ -3,5 +3,17 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "enemyData", menuName = "Scriptable Objects/enemyData")]
 public class enemyData : ScriptableObject
 {
+    public Enemies[] enemies;
     
 }
+
+[System.Serializable]
+public struct Enemies
+    {
+        public long enemy_id;
+        public long enemy_HP;
+        public string emeny_name;
+        public long enemy_attack;
+        public long enemy_speed;
+        public long enemy_attack_id;
+    }
